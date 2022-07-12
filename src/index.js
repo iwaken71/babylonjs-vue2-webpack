aab9f85eece15d67b1a9f156b524b5204237e7c6
iwaken71 <>
2022-07-12 20:14:38 +0900

    minimum

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 
 import Vue from 'vue'
-import Example from './components/example.vue'
 import BabylonScene from './components/BabylonScene.vue'
 
 const app = new Vue({
 	el: "#app",
 	components:{
-		Example,BabylonScene
-	},
-	data: {
-		message: 'Hello World'
+		BabylonScene
 	}
 })
